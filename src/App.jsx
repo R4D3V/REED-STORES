5f3511973c68f8a2d5f4ee8116871347eb79cbec
R4D3V <raymonjohns@gmail.com>
2024-01-24 19:06:51 +0300

    master

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import Footer from "./components/footer";
 import Header from "./components/header";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
-import About from "./pages/about";
+import Faq from "./pages/faq";
 import Contact from "./pages/contact";
 import Services from "./pages/services";
-import Portfolio from "./pages/portfolio";
+import Blog from "./pages/blog";
 const App = () => {
   return (
     <>
       <Header />
       <Routes>
         <Route path="" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<Faq />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/blog" element={<Blog />} />
         <Route path="/services" element={<Services />} />
       </Routes>
       <Footer />
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,53 +1,17 @@
-import { useTypewriter } from "react-simple-typewriter";
-import image from "../assets/android-chrome-512x512.png";
-import { Link } from "react-router-dom";
+
+// import image from "../assets/android-chrome-512x512.png";
+// import { Link } from "react-router-dom";
 
 
 function Home() {
-  const [typeEffect] = useTypewriter({
-    words: [""],
-    loop: {},
-    typeSpeed: 200,
-    deleteSpeed: 100,
-  });
+
+ 
+ 
   return (
-       <div className="flex flex-col md:flex-row mt-[82px] lg:mb-28 md:mb-28 ">
-        <div className="flex justify-center items-center">
-          <img
-            src={image}  
-            alt="r4d3v"
-            className="w-[50%] rounded-md"
-          />
-        </div>
-        <div className="p-[10%] lg:w-[50%] flex justify-center pb-6 relative flex-wrap">
-        <h1>Hi there you made it up to here!</h1>
-        <p className="mx-1 font-semibold text-2xl text-center">
-          I am Frontend Dev, Freelancer,Ui/Ux designer!
-          <span className="pl-2">{typeEffect}</span>
-        </p>
-       
-         <div className="flex flex-wrap justify-center gap-2 m-3">
-         <button className="flex items-center px-2 py-1  font-medium text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
-          <Link
-                 
-            to="/services">
-                  SERVICES
-                </Link>
-          </button>
-          <button className="flex items-center px-2 py-1 font-medium text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
-          <Link
-                 
-            to="/portfolio">
-                  PORTFOLIO
-                </Link>
-          </button>
-          <button className="flex items-center px-2 py-1 font-medium text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
-          <a href="https://dumani-media.vercel.app/products" target="_blank" rel="noopener noreferrer">HOT DEALS</a>
-          </button>
-         </div>
-        </div>
-      </div> 
-  );
+   <div>
+    <h1>homepage</h1>
+   </div>
+    );
 }
 
 export default Home;
