a9b199d5de437b228cfe62c144fc6331fdb5234d
R4D3V <raymonjohns@gmail.com>
2024-01-27 15:49:50 +0300

    main

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,8 +3,9 @@ import home from "../icons/home.png"
 
 function navbar() {
   return (
-    <nav className="container flex lg:hidden md:hidden sm:hidden items-center justify-center gap-4 h=[15px] flex-wrap fixed bottom-0 w-full bg-gray-400 text-white rounded-lg">
-        <Link to='/' className="p-4 text-sm transition-colors duration-300  hover:text-blue-500 dark:hover:text-blue-400">
+    <nav className="container  lg:hidden md:hidden sm:hidden ">
+       <div className="flex gap-4 flex-wrap fixed bottom-0 w-full justify-evenly bg-gray-400 text-white rounded-lg">
+       <Link to='/' className="p-4 text-sm transition-colors duration-300  hover:text-blue-500 dark:hover:text-blue-400">
             <img src={home} alt="" className="w-4 h-4"/>
             </Link>  
         <Link to='/products' className="p-4 text-sm transition-colors duration-300  hover:text-blue-500 dark:hover:text-blue-400">
@@ -14,7 +15,8 @@ function navbar() {
         <img src={home} alt="" className="w-4 h-4"/>
         </Link>
         <Link to='/testimonials' className="p-4 text-sm transition-colors duration-300  hover:text-blue-500 dark:hover:text-blue-400">
-        <img src={home} alt="" className="w-4 h-4"/></Link>      
+        <img src={home} alt="" className="w-4 h-4"/></Link> 
+        </div>     
     </nav>
   )
 }
