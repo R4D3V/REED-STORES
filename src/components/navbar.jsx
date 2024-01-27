798d1d5d72af9d7e169794b1a0f5bc4de64c8832
R4D3V <raymonjohns@gmail.com>
2024-01-27 15:11:21 +0300

    main

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ function navbar() {
   return (
     <nav className="container flex lg:hidden md:hidden sm:hidden items-center justify-center flex-wrap fixed bottom-0 w-full dark:bg-gray-950">
         <Link to='/terms' className="p-4 text-sm transition-colors duration-300  hover:text-blue-500 dark:hover:text-blue-400">Chats</Link>  
-        <Link to='/products className="p-4 text-sm transition-colors duration-300  hover:text-blue-500 dark:hover:text-blue-400"'>Updates</Link>
+        <Link to='/products' className="p-4 text-sm transition-colors duration-300  hover:text-blue-500 dark:hover:text-blue-400">Updates</Link>
         <Link to='/faq' className="p-4 text-sm transition-colors duration-300  hover:text-blue-500 dark:hover:text-blue-400">Calls</Link>
         <Link to='/testimonials' className="p-4 text-sm transition-colors duration-300  hover:text-blue-500 dark:hover:text-blue-400">Tools</Link>      
     </nav>
