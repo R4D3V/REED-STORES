5147bdb1e1a253eca44bac117a4f82054c06ef27
R4D3V <raymonjohns@gmail.com>
2024-01-28 16:13:50 +0300

    main

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
 import home from "../icons/home.png";
-import up from "../icons/up.png"
+import up from "../icons/up.png";
+import shop from "../icons/shop.png";
+import about from "../icons/about.png";
+import faq from "../icons/faq.png"
+
 
 function navbar() {
   return (
@@ -10,13 +14,19 @@ function navbar() {
             <img src={home} alt="" className="w-4 h-4"/>
             </Link>  
         <Link to='/products' className="p-4 text-sm transition-colors duration-300  hover:text-blue-500 dark:hover:text-blue-400">
-        <img src={home} alt="" className="w-4 h-4"/>
+        <img src={shop} alt="" className="w-4 h-4"/>
         </Link>
         <Link to='/faq' className="p-4 text-sm transition-colors duration-300  hover:text-blue-500 dark:hover:text-blue-400">
-        <img src={home} alt="" className="w-4 h-4"/>
+        <img src={faq} alt="" className="w-4 h-4"/>
         </Link>
-        <Link to='/testimonials' className="p-4 text-sm transition-colors duration-300  hover:text-blue-500 dark:hover:text-blue-400">
-        <img src={up} alt="" className="w-4 h-4"/><a href="#"></a></Link> 
+        <Link to='/about' className="p-4 text-sm transition-colors duration-300  hover:text-blue-500 dark:hover:text-blue-400">
+        <img src={about} alt="" className="w-4 h-4"/>
+        </Link>
+       
+        <div className="p-4 text-sm transition-colors duration-300  hover:text-blue-500 dark:hover:text-blue-400">
+        <a href="#"><img src={up} alt="#" className="w-4 h-4"/></a>
+        </div>
+        
         
       </div>     
     </nav>
