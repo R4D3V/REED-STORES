cb982c89bd1d50add625b68cb7bacc848a58d4c1
R4D3V <raymonjohns@gmail.com>
2024-02-01 14:00:58 +0300

    main

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,8 +4,8 @@ import image from "../images/logo/reed.png"
 
 function Home() {
   return (
-   <div className="mt-[60px]" >
-     <div className="flex-col bg-clip-border  bg-transparent text-gray-700 grid"> 
+   <div className="" >
+     <div className="flex-col bg-clip-border  bg-transparent text-gray-700 relative grid"> 
        <video className="w-full h-full object-cover" autoPlay loop muted>
         <source src={video} type="video/mp4" />
         Your
@@ -13,7 +13,7 @@ function Home() {
       <div className="absolute inset-0 bg-black/40 p-6 flex flex-col justify-end items-center">
       <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-white">Phones</h4>
         
-        <p className="block antialiased  text-center font-sans text-baseleading-relaxed text-white my-2 font-normal">We are excited to share that our bank has successfully expanded into new markets. With strategic partnerships and innovative solutions, we have established a strong presence in international markets. Learn more about our expansion journey and the opportunities it brings.</p>
+        <p className="block antialiased font-sans text-baseleading-relaxed text-white my-2 font-normal">We are excited to share that our bank has successfully expanded into new markets. With strategic partnerships and innovative solutions, we have established a strong presence in international markets. Learn more about our expansion journey and the opportunities it brings.</p>
         <Link to='/products' className="p-4 text-sm text-white bg-black rounded-full w-fit ">
         Check me out
         </Link>
